e5d784f52ba5ec9328e069255fcb3239b454bb95
taka2 <taka23131@gmail.com>
2022-08-23 21:25:32 +0900

    add: peg.jsのonline version相当の機能を付けた

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { Box, Button, Textarea } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { generate } from "pegjs";
+
 function App() {
-  return <div>Hello World</div>;
+  let [peg, setPeg] = useState("");
+  let [input, setInput] = useState("");
+  let [parser, setParser] = useState(generate('start = ""'));
+  let [result, setResult] = useState("");
+  return (
+    <>
+      <Box>grammer</Box>
+      <Textarea
+        placeholder="peg.js"
+        value={peg}
+        onChange={(e) => {
+          setPeg(e.target.value);
+          setParser(generate(e.target.value));
+        }}
+      />
+      <Box>input</Box>
+      <Textarea
+        placeholder="peg.js"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Button
+        onClick={() => {
+          setResult(parser.parse(input));
+        }}
+      >
+        Parse!
+      </Button>
+      <Box>result: {result}</Box>
+    </>
+  );
 }
 
 export default App;
