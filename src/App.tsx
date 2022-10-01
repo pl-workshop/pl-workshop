fa0796e8302934f5abb3c828f4a80a99e715eca8
taka2 <taka23131@gmail.com>
2022-10-01 15:39:04 +0900

    add: astのtreeの表示、evalのコードを書いて実行できる機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { Box, Button, Textarea } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import {
+  JSXElementConstructor,
+  ReactElement,
+  ReactFragment,
+  useState,
+} from "react";
 import { generate } from "pegjs";
-import Blockly from "blockly";
-import GrammerDefWorkspace from "./components/GrammerDefWorkspace";
+import { Tree, TreeNode } from "react-organizational-chart";
 
 function App() {
   let [input, setInput] = useState("");
-  let [parser, setParser] = useState(generate('start = ""'));
-  let [result, setResult] = useState("");
-  let [javascriptCode, setJavascriptCode] = useState("");
+  let [parser, setParser] = useState(
+    generate(initial_definition + integer_whitespace_definition)
+  );
+  let [result, setResult] = useState();
+  let [evalcode, setEvalcode] = useState(initial_evalcode);
+  let [evalResult, setEvalResult] = useState("");
   return (
     <>
       <Box>grammer</Box>
       <Textarea
         placeholder="peg.js"
+        defaultValue={initial_definition}
         onChange={(e) => {
-          setParser(generate(e.target.value));
+          console.log(e.target.value + integer_whitespace_definition);
+          setParser(generate(e.target.value + integer_whitespace_definition));
         }}
       />
       <Box>input</Box>
@@ -31,17 +40,70 @@ function App() {
       >
         Parse!
       </Button>
-      <Box>result: {result}</Box>
-      <Box>GrammerDefinition</Box>
-      <GrammerDefWorkspace
-        onWorkspaceChange={(workspace: any) => {
-          const code = Blockly.JavaScript.workspaceToCode(workspace);
-          setJavascriptCode(code);
+      <Box>result: {JSON.stringify(result)}</Box>
+      <Box>tree</Box>
+      {result && <Tree label={<div>Root</div>}>{ast2tree(result)}</Tree>}
+      <Box>eval</Box>
+      <Textarea
+        placeholder="eval"
+        defaultValue={initial_evalcode}
+        onChange={(e) => {
+          setEvalcode(e.target.value);
         }}
       />
-      <Box>code: {javascriptCode}</Box>
+      <Button
+        onClick={() => {
+          setEvalResult(
+            Function(evalcode + `;return eval(${JSON.stringify(result)});`)()
+          );
+        }}
+      >
+        eval!
+      </Button>
+      <Box>eval result: {evalResult}</Box>
     </>
   );
+}
+
+const initial_definition = `start = Expression
+Expression = head: Factor tail: ("+" Factor)* {
+return tail.reduce((result, element) => {return {tag: "Add", lh: result, rh: element[1]}}, head)
+}
+
+Factor = num: Integer {
+return {tag: "Number", value: num}
+}`;
+
+const integer_whitespace_definition = `
+Integer "integer"
+  = _ [0-9]+ { return parseInt(text(), 10); }
+
+_ "whitespace"
+  = [ \\t\\n\\r]*
+`;
+
+const initial_evalcode = `const eval = (ast) => {
+    switch (ast.tag) {
+        case "Number": return ast.value
+    }
+}`;
+
+type AST = { tag: string } | number;
+
+function ast2tree(ast: any) {
+  if (typeof ast === "number") {
+    return <TreeNode label={<div>{ast}</div>} />;
+  } else {
+    return (
+      <TreeNode label={<div>{ast.tag}</div>}>
+        {Object.keys(ast)
+          .filter((key: string) => key !== "tag")
+          .map((key: string) => (
+            <TreeNode label={<div>{key}</div>}>{ast2tree(ast[key])}</TreeNode>
+          ))}
+      </TreeNode>
+    );
+  }
 }
 
 export default App;
