5c5e34f61739294c40670c5eded2733a233b1940
taka2 <taka23131@gmail.com>
2022-10-03 01:25:17 +0900

    add: レイアウトの調整をした

---
+++ src/assets/components/ResultBox.tsx
@@ -0,0 +1,24 @@
+import { Box, Text } from "@chakra-ui/react";
+
+type Props = {
+  label: string;
+  children: string;
+};
+export default function ResultBox({ label, children }: Props) {
+  return (
+    <Box>
+      <Text
+        paddingX={2}
+        paddingY={1}
+        borderTop="1px"
+        borderX="1px"
+        fontSize="2xl"
+      >
+        {label}
+      </Text>
+      <Text padding={2} fontSize="lg" border="1px" minH={50}>
+        {children}
+      </Text>
+    </Box>
+  );
+}
