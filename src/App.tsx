97f6f27e449d91c58afd9aedc2f2e0ad130d5810
taka2 <taka23131@gmail.com>
2022-11-18 19:04:22 +0900

    formatを変えた

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {
     Text,
-        Box,
-        Button,
-        Textarea,
-        Flex,
-        Stack,
-        Center,
-        Divider,
+    Box,
+    Button,
+    Textarea,
+    Flex,
+    Stack,
+    Center,
+    Divider,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { generate } from "pegjs";
@@ -23,125 +23,127 @@ function App() {
     let [evalcode, setEvalcode] = useState(initial_evalcode);
     let [evalResult, setEvalResult] = useState("");
     return (
-            <Stack h="100vh">
+        <Stack h="100vh">
             <Flex h="full" gap={4} paddingX={4} paddingY={2}>
-            <Stack spacing={8} width="full" height="full">
-            <Stack spacing={2}>
-            <Text fontSize="3xl" color="blackAlpha.900">
-            文法定義部
-            </Text>
-            <AceEditor
-            mode="javascript"
-            theme="chrome"
-            value={parserDef}
-            width="100%"
-            minLines={20}
-            maxLines={20}
-            readOnly={false}
-            fontSize={16}
-            enableBasicAutocompletion={true}
-            onChange={(s) => {
-            setParserDef(s);
-            }}
-            />
-          </Stack>
-          <Stack spacing={2} width="full" h="full">
-            <Text fontSize="3xl" color="blackAlpha.900">
-              入力
-            </Text>
-            <Textarea
-              h="full"
-              maxH={200}
-              placeholder="peg.js"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-                <Button
-                colorScheme="green"
-                w={20}
-            minH={10}
-            onClick={() => {
-                try {
-                    const parser = generate(
-                            parserDef + integer_whitespace_definition
-                            );
-                    setResult(parser.parse(input));
-                } catch (e) {
-                    alert(e);
-                }
-              }}
-            >
-              Parse!
-            </Button>
-          </Stack>
-        </Stack>
-        <Stack maxW="50%" width="full" h="full">
-          <ResultBox label="構文解析の結果">{JSON.stringify(result)}</ResultBox>
-          <Box>
-            <Text
-              fontSize="2xl"
-              paddingX={2}
-              paddingY={1}
-              borderTop="1px"
-              borderX="1px"
-              borderColor="green"
-            >
-              AST Viewer
-            </Text>
-            <Box
-              overflow="scroll"
-              padding={2}
-              border="1px"
-              borderColor="green"
-              minH={300}
-              maxH={375}
-            >
-              {result && (
-                <Tree label={<div>Root</div>}>{ast2tree(result)}</Tree>
-              )}
-            </Box>
-                </Box>
+                <Stack spacing={8} width="full" height="full">
+                    <Stack spacing={2}>
+                        <Text fontSize="3xl" color="blackAlpha.900">
+                            文法定義部
+                        </Text>
+                        <AceEditor
+                            mode="javascript"
+                            theme="chrome"
+                            value={parserDef}
+                            width="100%"
+                            minLines={20}
+                            maxLines={20}
+                            readOnly={false}
+                            fontSize={16}
+                            enableBasicAutocompletion={true}
+                            onChange={(s) => {
+                                setParserDef(s);
+                            }}
+                        />
+                    </Stack>
+                    <Stack spacing={2} width="full" h="full">
+                        <Text fontSize="3xl" color="blackAlpha.900">
+                            入力
+                        </Text>
+                        <Textarea
+                            h="full"
+                            maxH={200}
+                            placeholder="peg.js"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                        />
+                        <Button
+                            colorScheme="green"
+                            w={20}
+                            minH={10}
+                            onClick={() => {
+                                try {
+                                    const parser = generate(
+                                        parserDef + integer_whitespace_definition
+                                    );
+                                    setResult(parser.parse(input));
+                                } catch (e) {
+                                    alert(e);
+                                }
+                            }}
+                        >
+                            Parse!
+                        </Button>
+                    </Stack>
                 </Stack>
-                </Flex>
-                <Divider />
-                <Flex gap={4} paddingX={4} paddingBottom={4}>
+                <Stack maxW="50%" width="full" h="full">
+                    <ResultBox label="構文解析の結果">{JSON.stringify(result)}</ResultBox>
+                    <Box maxH="70%">
+                        <Text
+                            fontSize="2xl"
+                            paddingX={2}
+                            paddingY={1}
+                            borderTop="1px"
+                            borderX="1px"
+                            borderColor="green"
+                        >
+                            AST Viewer
+                        </Text>
+                        <Box
+                            overflow="scroll"
+                            padding={2}
+                            border="1px"
+                            borderColor="green"
+                            minH={300}
+                            maxH={375}
+                        >
+                            {result && (
+                                <Tree label={<div>Root</div>}>{ast2tree(result)}</Tree>
+                            )}
+                        </Box>
+                    </Box>
+                </Stack>
+            </Flex>
+            <Divider />
+            <Flex gap={4} paddingX={4} paddingBottom={4}>
                 <Stack spacing={2} width="full">
-                <Text fontSize="3xl" color="blackAlpha.900">
-                評価部
-                </Text>
-                <AceEditor
-                mode="javascript"
-                theme="chrome"
-                value={evalcode}
-            width="100%"
-                minLines={16}
-            maxLines={16}
-            readOnly={false}
-            fontSize={16}
-            enableBasicAutocompletion={true}
-            onChange={(s) => {
-                setEvalcode(s);
-            }}
-            />
-                <Button
-                w={20}
-            h={10}
-            colorScheme="green"
-                onClick={() => {try {
-                    setEvalResult(Function(evalcode + `;return eval;`)()(result));
-                } catch (e) {
-                    alert(e)
-                }}}
-            >
-                Eval!
-                </Button>
+                    <Text fontSize="3xl" color="blackAlpha.900">
+                        評価部
+                    </Text>
+                    <AceEditor
+                        mode="javascript"
+                        theme="chrome"
+                        value={evalcode}
+                        width="100%"
+                        minLines={16}
+                        maxLines={16}
+                        readOnly={false}
+                        fontSize={16}
+                        enableBasicAutocompletion={true}
+                        onChange={(s) => {
+                            setEvalcode(s);
+                        }}
+                    />
+                    <Button
+                        w={20}
+                        h={10}
+                        colorScheme="green"
+                        onClick={() => {
+                            try {
+                                setEvalResult(Function(evalcode + `;return eval;`)()(result));
+                            } catch (e) {
+                                alert(e)
+                            }
+                        }}
+                    >
+                        Eval!
+                    </Button>
                 </Stack>
                 <Stack width="full">
-                <ResultBox label="評価結果">{evalResult.toString()}</ResultBox>
+                    <ResultBox label="評価結果">{evalResult.toString()}</ResultBox>
                 </Stack>
-                </Flex>
-                </Stack>
-                );
+            </Flex>
+        </Stack>
+    );
 }
 
 const initial_definition = `start = Expression
@@ -168,39 +170,39 @@ const initial_evalcode = `const eval = (ast) => {
 }`;
 
 type AST =
-| { tag: string; [key: string]: AST }
-| number
-| string
-| boolean
-| AST[];
+    | { tag: string;[key: string]: AST }
+    | number
+    | string
+    | boolean
+    | AST[];
 
 function ast2tree(ast: AST) {
     if (
-            typeof ast === "number" ||
-            typeof ast === "string" ||
-            typeof ast === "boolean"
-       ) {
+        typeof ast === "number" ||
+        typeof ast === "string" ||
+        typeof ast === "boolean"
+    ) {
         return <TreeNode label={<Text>{ast}</Text>} />;
     } else if (ast instanceof Array) {
         return (
-                <>
+            <>
                 {ast.map((x, index) => (
-                            <TreeNode key={index} label={<>{ast2tree(x)}</>} />
-                            ))}
-                </>
-               );
+                    <TreeNode key={index} label={<>{ast2tree(x)}</>} />
+                ))}
+            </>
+        );
     } else {
         return (
-                <TreeNode label={<Text color="brown">{ast.tag}</Text>}>
+            <TreeNode label={<Text color="brown">{ast.tag}</Text>}>
                 {Object.keys(ast)
-                .filter((key: string) => key !== "tag")
-                .map((key: string, index) => (
-                            <TreeNode key={index} label={<Text color="green">{key}</Text>}>
+                    .filter((key: string) => key !== "tag")
+                    .map((key: string, index) => (
+                        <TreeNode key={index} label={<Text color="green">{key}</Text>}>
                             {ast2tree(ast[key])}
-                            </TreeNode>
-                            ))}
-                </TreeNode>
-               );
+                        </TreeNode>
+                    ))}
+            </TreeNode>
+        );
     }
 }
 
